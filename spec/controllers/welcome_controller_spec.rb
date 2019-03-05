8af231a45c31b780a6104db68ec0f3e1ad9094d4
William Bode <william.bode@gmx.de>
2019-03-05 19:33:38 +0100

    add pre commit hook

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  describe "GET index" do
-    it "renders the index template" do
+  describe 'GET index' do
+    it 'renders the index template' do
       get :index
       expect(response.body).to eq({ versions: ['v1'] }.to_json)
     end
